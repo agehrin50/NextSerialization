0315914429cdfa31f34359af96f12056d6b5f5c3
Erik Gabbard <egabbard@diagraph.com>
2022-04-06 09:25:31 -0500

    Moved docfx output to /docs folder

--- NextSerialization/MessageTypes/NextMessage/Interfaces/IFieldObject.cs
+++ NextSerialization/MessageTypes/NextMessage/Interfaces/IFieldObject.cs
@@ -1,32 +1,34 @@
 ﻿namespace NextSerialization.MessageTypes.NextMessage.Interfaces;
 
 /// <summary>
-/// 
+/// Defines properties that are intrinsic to any element to be rendered.
 /// </summary>
 public interface IFieldObject
 {
     /// <summary>
-    /// 
+    /// The horizontal distance that the element will be rendered measured, in inches, from the top left corner of the message.
     /// </summary>
     public decimal X { get; set; }
 
     /// <summary>
-    /// 
+    /// The vertical distance that the element will be rendered measured, in inches, from the top left corner of the message.
     /// </summary>
     public decimal Y { get; set; }
 
     /// <summary>
-    /// 
+    /// Indicates that the element's aspect ratio should be maintained when resizing.
     /// </summary>
     public string LockAspectRatio { get; set; }
 
     /// <summary>
-    /// 
+    /// The vertical measurement of the size of the element.
     /// </summary>
+    /// <remarks>The units of measure for the <see cref="Height"/> could be localized and may appear in imperial or metric form.</remarks>
     public decimal Height { get; set; }
 
     /// <summary>
-    /// 
+    /// Function 1 Symbol Character used as a data separator in GS1 barcodes.
     /// </summary>
+    /// <remarks>Note: <see cref="FNC1"/> is only used when a <see cref="FieldObject"/> is a sub-element of a <see cref="BarcodeField"/> element.</remarks>
     public string FNC1 { get; set; }
 }
--- NextSerialization/MessageTypes/NextMessage/Models/FieldObject.cs
+++ NextSerialization/MessageTypes/NextMessage/Models/FieldObject.cs
@@ -34,38 +34,41 @@ using NextSerialization.MessageTypes.NextMessage.Interfaces;
 public abstract class FieldObject : IFieldObject
 {
     /// <summary>
-    /// 
+    /// The horizontal distance that the element will be rendered measured, in inches, from the top left corner of the message.
     /// </summary>
     [XmlAttribute]
     public decimal X { get; set; }
 
     /// <summary>
-    /// 
+    /// The vertical distance that the element will be rendered measured, in inches, from the top left corner of the message.
     /// </summary>
     [XmlAttribute]
     public decimal Y { get; set; }
 
     /// <summary>
-    /// 
+    /// The horizontal measurement of the size of the element.
     /// </summary>
+    /// <remarks>The units of measure for the <see cref="Width"/> could be localized and may appear in imperial or metric form.</remarks>
     [XmlAttribute]
     public virtual decimal Width { get; set; }
 
     /// <summary>
-    /// 
+    /// The vertical measurement of the size of the element.
     /// </summary>
+    /// <remarks>The units of measure for the <see cref="Height"/> could be localized and may appear in imperial or metric form.</remarks>
     [XmlAttribute]
     public decimal Height { get; set; }
 
     /// <summary>
-    /// 
+    /// Indicates that the element's aspect ratio should be maintained when resizing.
     /// </summary>
     [XmlAttribute]
     public string LockAspectRatio { get; set; }
 
     /// <summary>
-    /// 
+    /// Function 1 Symbol Character used as a data separator in GS1 barcodes.
     /// </summary>
+    /// <remarks>Note: <see cref="FNC1"/> is only used when a <see cref="FieldObject"/> is a sub-element of a <see cref="BarcodeField"/> element.</remarks>
     [XmlAttribute]
     public virtual string FNC1 { get; set; }
 }
